d0a55f70a495b89b90a0675cc3da70b0d7cd8c06
1000peach <_970813@daum.net>
2019-08-21 21:02:53 +0900

    작업 위한 db 비번 수정

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ var fs = require("fs-extra");
 var connection = mysql.createConnection({
   host : 'localhost',
   user : 'nodejs',
-  password : 'nodejs', // 각자 nodejs가 사용할 user, password로 변경 후 작업
+  password : '00000000', // 각자 nodejs가 사용할 user, password로 변경 후 작업
   // port : 3306,
   database : 'project',
   charset  : 'utf8'
