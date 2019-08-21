2811a8897263121fd7ae3092dad1897bf8451fc9
1000peach <_970813@daum.net>
2019-08-21 20:47:45 +0900

    홈페이지 디자인 변경
    - main 페이지 디자인 전체적으로 틀 잡기-> 임시 이미지로 변경 (데이터와
    대응되는 이미지는 기능 구현에서 수정 예정)
    - contents 페이지 상단 디자인 수정

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,12 @@
 var login = document.getElementById('login');
 var register = document.getElementById('register');
+var popular = document.getElementsByClassName("popular");
+
+// for (var i = 0; i < popular.length; i++) {
+//     if ( (i%2)!=0 ) {
+//         popular[i].style.background = '#F6F6F6';
+//     }
+// }
 
 function logIn() {
     location.href = "/login";
